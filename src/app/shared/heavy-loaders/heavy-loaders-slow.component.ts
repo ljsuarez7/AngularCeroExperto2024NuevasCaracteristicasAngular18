bb02d00e0fae0dc1f709820e5bf03e718a895bf8
Javier Suárez <ljsuarez7@gmail.com>
2024-08-03 11:43:42 +0200

    configurado tailwind y creada estructura base del proyecto y los componentes

---
+++ src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heavy-loaders-slow',
+  standalone: true,
+  imports: [],
+  template: `
+    <h1>app-heavy-loaders-slow</h1>
+  `
+})
+export class HeavyLoadersSlowComponent {
+
+}
---
+++ src/app/shared/heavy-loaders/users-loader.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users-loader',
+  standalone: true,
+  imports: [],
+  template: `
+    <h1>app-users-loader</h1>
+  `
+})
+export class UsersLoaderComponent {
+
+}
