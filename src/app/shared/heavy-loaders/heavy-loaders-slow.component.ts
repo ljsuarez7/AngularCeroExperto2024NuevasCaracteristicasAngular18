efc532f8f0b6e5d208274710cfe1be76d78806ed
Javier Suárez <ljsuarez7@gmail.com>
2024-08-06 19:54:35 +0200

    componentes defer-views y heavy loader slow creados

--- src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
+++ src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-heavy-loaders-slow',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   template: `
-    <h1>app-heavy-loaders-slow</h1>
+    <section [ngClass]="['w-full h-[600px]', cssClass]">
+      Heavy Loaders Slow
+    </section>
   `
 })
 export class HeavyLoadersSlowComponent {
+
+  @Input({required: true}) cssClass!: string;
+
+  constructor(){
+
+    const start = Date.now();
+
+    while(Date.now() - start < 3000){}
+
+    console.log('cargado');
+
+  }
 
 }
