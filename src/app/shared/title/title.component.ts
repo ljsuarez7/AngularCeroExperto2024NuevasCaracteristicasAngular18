66b3f4f8d1523df01ee7bcae4f929d731414d01b
Javier Suárez <ljsuarez7@gmail.com>
2024-08-06 19:53:15 +0200

    componente title creado

--- src/app/shared/title/title.component.ts
+++ src/app/shared/title/title.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { booleanAttribute, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   standalone: true,
   imports: [],
-  templateUrl: './title.component.html',
+  template: `
+    <!-- <h1 class="text-3xl mb-5">{{title}} - {{withShadow}}</h1> -->
+    <h1 class="text-3xl mb-5">{{title}}</h1>
+  `,
   styles: ``
 })
 export class TitleComponent {
+
+  @Input({required: true}) title!: string;
+
+  //Con el transform hacemos q se pueda mandar true solo poniendo el nombre del input en el componente
+  //Si lo tiene será true, sino será false
+  // @Input({transform: booleanAttribute}) withShadow: boolean = false;
 
 }
